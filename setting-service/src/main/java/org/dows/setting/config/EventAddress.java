2923ffd8b546f078ed61064fa71d767b07aa86ac
Lait <lait.zhang@gmail.com>
2026-01-02 15:57:55 +0800

    refactor(setting): 移除未使用的事件和Verticle相关代码
    - 删除了AttachmentDownloadedEvent事件类
    - 移除了Codec消息编解码器
    - 删除了EventAddress注解
    - 移除了OrderVerticle和ResumeFileVerticle相关代码
    - 清理了pom.xml中相关的依赖配置
    - 移除了VerticleBootstrap配置类

--- setting-service/src/main/java/org/dows/setting/config/EventAddress.java
+++
@@ -1,9 +0,0 @@
-//package org.dows.setting.config;
-//
-//import java.lang.annotation.*;
-//
-//@Target({ElementType.TYPE})
-//@Retention(RetentionPolicy.RUNTIME)
-//@Documented
-//public @interface EventAddress {
-//}
